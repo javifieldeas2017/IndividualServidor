e330606bd3f0c72630324c806ed5c3bfe8e2f467
unknown <javifieldeas2017@outlook.es>
2017-09-13 16:19:30 +0200

    Cambio en préstamo

--- individual_net/Prestamo.cs
+++ individual_net/Prestamo.cs
@@ -8,12 +8,11 @@ namespace individual_net
     public class Prestamo
     {
         public long Id { get; set; }
-        public int IdPrestamo { get; set; }
+        public long IdPrestamo { get; set; }
         public string NIF { get; set; }
-        public string ISBN { get; set; }
-        public string ISAN { get; set; }
-        public string ISMN { get; set; }
-        public int idCopia { get; set; }
+        public long ISBN { get; set; }
+        public long ISAN { get; set; }
+        public long ISMN { get; set; }
         public string Recepcion { get; set; }
         public string Devolucion { get; set; }
     }
